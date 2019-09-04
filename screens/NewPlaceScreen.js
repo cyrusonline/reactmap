2afcee3b56c172b9c8ecad71f203bd4f079ccf9b
cyrusonline <online_mad@yahoo.com.hk>
2019-09-04 23:29:47 +0800

    screen and navigation setup

--- screens/NewPlaceScreen.js
+++ screens/NewPlaceScreen.js
@@ -1,15 +1,38 @@
 
 import React from 'react';
-import {View, Text, StyleSheet} from 'react-native'
+import {ScrollView, View, Button, Text, TextInput,StyleSheet} from 'react-native'
+import  Colors from '../constants/Colors';
 
 const NewPlaceScreen = props =>{
     return (
-        <View>
-            <Text>NewPlaceScreen</Text>
+        <ScrollView>
+        <View style={styles.form}>
+            <Text style={styles.label}>NewPlaceScreen</Text>
+            <TextInput style={styles.textInput}/>
+            <Button title='Save Place' color={Colors.primary} onPress={()=>{}}/>
         </View>
+        </ScrollView>  
     )
 }
+NewPlaceScreen.navigationOptions = {
+    headerTitle: 'Add Places'
+}
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    form:{
+        margin:30
+    },
+    label:{
+        fontSize:18,
+        marginBottom: 15
+    },
+    textInput:{
+        borderBottomColor: '#ccc',
+        borderBottomWidth:1,
+        marginBottom:15,
+        paddingVertical:4,
+        paddingHorizontal: 2
+    }
+})
 
 export default NewPlaceScreen
--- screens/PlacesListScreen.js
+++ screens/PlacesListScreen.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import {View, Text, StyleSheet} from 'react-native'
-
+import {HeaderButtons, Item} from 'react-navigation-header-buttons'
+import HeaderButton from '../components/HeaderButton'
+import { Platform } from '@unimodules/core';
 const PlacesListScreen = props =>{
     return (
         <View>
@@ -8,6 +10,24 @@ const PlacesListScreen = props =>{
         </View>
     )
 }
+PlacesListScreen.navigationOptions = navData=>{
+    return{
+        headerTitle: 'All Places',
+        headerRight:(
+            <HeaderButtons HeaderButtonComponent={HeaderButton}>
+                <Item
+                    title='Add Place'
+                    iconName={Platform.OS === 'android' ? 'md-add':'ios-add'}
+                    onPress = {()=>{
+                        navData.navigation.navigate('NewPlace')
+                    }}
+                />
+            </HeaderButtons>
+        )
+    }
+}
+
+
 
 const styles = StyleSheet.create({})
 
