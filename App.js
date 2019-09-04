1297878fa5415aa1b0eddcedda8fc93db59bb5f4
Cyrus Chan <cyruschan@hsu.edu.hk>
2019-09-04 17:29:06 +0800

    reactmap

---
+++ App.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+import PlacesNavigator from './navigation/PlacesNavigator'
+export default function App() {
+  return <PlacesNavigator/>;
+}
