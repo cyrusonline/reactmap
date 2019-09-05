eb06f657d261511d4aa4784daba0e8425b6bee78
cyrusonline <online_mad@yahoo.com.hk>
2019-09-05 23:52:49 +0800

    update at home

--- components/ImagePicker.js
+++ components/ImagePicker.js
@@ -1,12 +1,37 @@
 import React from 'react';
-import {View, Button, Text,StyleSheet } from 'react-native';
+import {View, Button,Image, Text,StyleSheet } from 'react-native';
 import Colors from '../constants/Colors';
-const ImagePicker = props => {
-    const takeImageHandler = () =>{};
-    return <View style={styles.imagePicker}> 
-        <View style={styles.imagePicker}><Text>No image picked yet</Text></View>
+import * as ImagePicker from 'expo-image-picker';
+const ImgPicker = props => {
+    const takeImageHandler = () =>{
+        ImagePicker.launchCameraAsync()
+    };
+    return (<View style={styles.imagePicker}> 
+        <View style={styles.imagePicker}>
+            <Text>No image picked yet</Text>
+            <Image style={styles.image}/>>
+            </View>
         <Button title="Take Image" color={Colors.primary} onPress={takeImageHandler}/>
-    </View>
+    </View>)
 };
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    imagePicker:{
+        alignItems:'center'
+    },
+    imagePreview:{
+        width:'100%',
+        height:200,
+        marginBottom:10,
+        justifyContent:'center',
+        alignItems:'center',
+        borderColor:'#ccc'
+    },
+    image:{
+        width:'100%',
+        height:'100%'
+    }
+
+})
+
+export default ImgPicker;
